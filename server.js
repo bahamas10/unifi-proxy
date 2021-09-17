54770e3db50591d3ce4613ad8f502fbfca1be871
rick51231 <rick51231@users.noreply.github.com>
2021-09-17 11:54:51 +0300

    Ability to use nemesis.

--- server.js
+++ server.js
@@ -9,11 +9,13 @@
 
 var fs = require('fs');
 var dgram = require('dgram');
+const exec = require('child_process').exec;
 
 // unifi udp discovery stuff
 var port = 10001;
 var mcastAddr = '233.89.188.1';
 var request = Buffer.from([1, 0, 0, 0]);
+const use_nemesis = false;
 
 var packetFile = process.argv[2];
 if (!packetFile) {
@@ -21,6 +23,11 @@ if (!packetFile) {
     process.exit(1);
 }
 var message = Buffer.from(JSON.parse(fs.readFileSync(packetFile, 'utf8')));
+const controller_ip = message[13]+'.'+message[14]+'.'+message[15]+'.'+message[16];
+
+if(use_nemesis) {
+	fs.writeFileSync('unifi-server.packet', message);
+}
 
 var socket = dgram.createSocket({
     type: 'udp4',
@@ -33,6 +40,10 @@ socket.on('message', function (msg, rinfo) {
     if (!msg.equals(request)) {
         console.warn('ignoring non-request packet');
         return;
+    }
+
+    if(use_nemesis) {
+    	exec("nemesis udp -c 1 -x 10001 -y " + rinfo.port + " -P unifi-server.packet -S " + controller_ip + " -D " + rinfo.address);
     }
 
     socket.send(message, 0, message.length, rinfo.port, rinfo.address, function (err, bytes) {
